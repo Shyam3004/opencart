32c85392200b9c3400cd57e3b55ce2f55661e471
Shyam <shyam30042000@gmail.com>
2022-06-16 23:35:42 +0530

    wishlist test and home page objects

--- src/pageobjects/HomePage.java
+++ src/pageobjects/HomePage.java
@@ -1,11 +1,14 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class HomePage {
+import base.BaseClass;
+
+public class HomePage extends BaseClass{
 	
 	public HomePage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
@@ -23,6 +26,38 @@ public class HomePage {
 		
 	@FindBy(xpath="//ul[@class='dropdown-menu dropdown-menu-right']//a[contains(text(),'Logout')]")
 	WebElement logout;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'Desktops')]")
+	public WebElement desktopsBtn;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'Desktops')]/parent::li//a[contains(text(),'Mac')]")
+	public WebElement categoryMacDesktop;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'Components')]/parent::div")
+	public WebElement componentsBtn;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'Components')]/parent::li//a[contains(text(),'Monitors')]")
+	public WebElement categoryMonitors;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'Tablets')]")
+	public WebElement categoryTablet;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'Desktops')]/parent::li//a[contains(text(),'Show All Desktops')]")
+	public WebElement categoryAllDesktops;
+	
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'MP3 Players')]/parent::li")
+	public WebElement mp3PlayersBtn;
+	
+	@FindBy(xpath = "//nav[@id='menu']//a[contains(text(),'MP3 Players')]/parent::li//a[contains(text(),'Show All MP3 Players')]")
+	public WebElement categoryMP3Players;
+	
+	
+	public void wishlistProduct(String productName) {
+		WebElement wishlistBtn = driver.findElement(By.xpath("//a[contains(text(),'"+productName+"')]/ancestor::div[@class='product-thumb']//button[@data-original-title='Add to Wish List']"));
+		wishlistBtn.click();
+	}
+	
 	
 
 }
---
+++ src/testcases/AddingWishlistTest.java
@@ -0,0 +1,35 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+import pageobjects.HomePage;
+
+public class AddingWishlistTest extends BaseClass{
+	
+	HomePage homeObj;
+	
+	@Test
+	public void wishlistByCategories() {
+		homeObj = new HomePage(driver);
+		homeObj.desktopsBtn.click();
+		homeObj.categoryAllDesktops.click();
+		homeObj.wishlistProduct("MacBook Air");
+		homeObj.wishlistProduct("HTC Touch HD");
+		homeObj.wishlistProduct("iPhone");
+		homeObj.componentsBtn.click();
+		homeObj.categoryMonitors.click();
+		homeObj.wishlistProduct("Apple Cinema 30\"");
+		homeObj.categoryTablet.click();
+		homeObj.wishlistProduct("Samsung Galaxy Tab 10.1");
+		homeObj.desktopsBtn.click();
+		homeObj.categoryMacDesktop.click();
+		homeObj.wishlistProduct("iMac");
+		homeObj.mp3PlayersBtn.click();
+		homeObj.categoryMP3Players.click();
+		homeObj.wishlistProduct("iPod Classic");
+		homeObj.wishlistProduct("iPod Touch");
+		
+	}
+	
+}
